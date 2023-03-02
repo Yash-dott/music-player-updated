d09b1b297fc66c235877c6e6c2aa29a96ab2817a
Yashtechnosters <100863383+Yash-dott@users.noreply.github.com>
2023-03-03 00:15:39 +0530

    music-player-updated

---
+++ assets/js/audio.js
@@ -0,0 +1,38 @@
+const songs = [
+    {
+        filename: 1,
+        songname: "Lost sky",
+        singername: "feat. Chris Linton",
+
+    },
+    {
+        filename: 2,
+        songname: "Billian Billian",
+        singername: "Satti Dhillon",
+
+    },
+    {
+        filename: 3,
+        songname: "Lil Nas X INDUSTRY BABY",
+        singername: "Lil Nas X Jack Harlow",
+
+    },
+    {
+        filename: 4,
+        songname: "nbw",
+        singername: "jhgs",
+
+    },
+    {
+        filename: 5,
+        songname: "nbw",
+        singername: "jhgs",
+
+    },
+    {
+        filename: 6,
+        songname: "nbw",
+        singername: "jhgs",
+
+    },
+]
